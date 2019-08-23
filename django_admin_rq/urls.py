41201bdfcfce0460e49e1352dc8cfb1ce7deb58d
David Baum <780938+serjant@users.noreply.github.com>
2019-08-23 20:29:41 +0300

    Update urls.py

--- django_admin_rq/urls.py
+++ django_admin_rq/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from django_admin_rq import views
 
 urlpatterns = [
-    path('job/status/<string:job_uuid>/', views.JobStatusView.as_view(), name='admin-rq-job-status'),
+    path('job/status/<str:job_uuid>/', views.JobStatusView.as_view(), name='admin-rq-job-status'),
 ]
