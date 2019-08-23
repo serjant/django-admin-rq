90cc6747de77d110f0539ac4c8c33235a8c794c9
David Baum <780938+serjant@users.noreply.github.com>
2019-08-23 20:28:12 +0300

    Fixed the url pattern for the Django>=2.0

--- django_admin_rq/urls.py
+++ django_admin_rq/urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url
+from django.urls import path
 from django_admin_rq import views
 
 urlpatterns = [
-    url(r'^job/status/(?P<job_uuid>[a-zA-Z0-9-_]+)/', views.JobStatusView.as_view(), name='admin-rq-job-status'),
+    path('job/status/<string:job_uuid>/', views.JobStatusView.as_view(), name='admin-rq-job-status'),
 ]
